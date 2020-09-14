ad98c84957002bca8e071c4ccb2114219195ec7f
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2020-09-14 19:39:56 -0400

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-poetry run gunicorn "animalflix:create_app()"
+if ! command -v poetry &> /dev/null
+then
+    gunicorn "animalflix:create_app()"
+    exit
+else
+    poetry run gunicorn "animalflix:create_app()"
+fi
