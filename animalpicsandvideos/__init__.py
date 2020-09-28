c8fbfae382d887f575a9ce452fe80e1499580be2
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2020-09-27 21:34:48 -0400

    Implement CORS

--- animalpicsandvideos/__init__.py
+++ animalpicsandvideos/__init__.py
@@ -3,11 +3,13 @@ __version__ = "0.1.0"
 
 import animalpicsandvideos.animals
 from flask import Flask
+from flask_cors import CORS
 from .json_encoder import AnimalPicsAndVideosJSONEncoder
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    CORS(app)
     app.json_encoder = AnimalPicsAndVideosJSONEncoder
 
     app.register_blueprint(
